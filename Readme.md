616c4616b56448b0fe389b4fde77d93558d894a6
Anand Mahesh <manand881@gmail.com>
2020-09-19 12:58:03 +0530

    v1.0

---
+++ Readme.md
@@ -0,0 +1,33 @@
+# System Stressor
+
+This is a simple script i wrote out of curiosity while learning about multiprocessors and random number generators
+
+##About
+
+This program is designed to consume all your RAM, CPU and Secondary Memory(HDD/SSD) without actually giving you anything back in return, in the same order as mentioned earlier.
+
+## Installation
+
+Install [python](https://www.python.org/downloads/) or follow a suitable tutorial if you're having problems. I would strongly recommend using python 64 since the instruction set is twice as wide.
+
+## Usage
+
+* Can be used to stress test a machine to understand how it reacts under load.
+* Can be used to monitor battery drain of a laptop under load.
+
+## Vulnerability
+
+* If this script is loaded into the start up folder in windows or any comparable os which has such a directory, it can render a machine useless from system startup.
+
+* Running this script even voluntarily is damaging when you have an SSD as a primary boot drive since it forces the system to write page files to the disk which will later be erased once the script is exited. The life span of an SSD is measured by the total size of data written on it. Since the program writes data on your SSD which will later be dumped, it is reducing the life span of the device
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+You can also reach me at this [Mail](manand881@gmail.com) address.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- stressor.py
+++ stressor.py
@@ -1,32 +1,42 @@
 import random
 import time
-import threading
 import multiprocessing
 from multiprocessing import Process
 
 # Arbeit macht frei is a German phrase meaning "Work sets you free." The slogan is known for appearing on the entrance of Auschwitz and other Nazi concentration camps.
 
-Logical_cores = multiprocessing.cpu_count()
+Logical_cores = multiprocessing.cpu_count()         # counting the number of logical cores in the system
+Pros = []                                           # declaring a variable which acts as a list of processes
 
 
-def work(x):
+def work():
+    zero = list()                                   # creating a list to hold floating point operation results to stress memory
     while True:
-        one = float()
+        one = float()                               # creating empty variables later to be used as an extra step before it is reassigned in the random function
         two = float()
         three = float()
         four = float()
 
-        one = random.random()
+        one = random.random()                       # storing random floating point values
         two = random.random()
         three = random.random()
         four = random.random()
 
-        five = one * two
+        five = one * two                            # distilling it down to two values and adding an extra step in the between
         six = three * four
 
-        print(five / six,x)
+        zero.append(five + six)                     # performing floating point operations to stress CPu
+        zero.append(five - six)
+        zero.append(five * six)
+        zero.append(five / six)
 
 
-for x in range(Logical_cores):
-    p = Process(target=work, args=(x,))
-    p.start()
+if __name__ == '__main__':
+    for x in range(Logical_cores):                  # creating processes to match the number of logical cores of the system
+        p = Process(target=work)
+        time.sleep(3)                               # giving the system enough time to reorganise itself and create page files for other programs.
+        Pros.append(p)                              # appending list of processes to the before mentioned variable
+        p.start()                                   # staring the processes
+
+for t in Pros:                                      #   The liberation that shall never come
+    t.join()
